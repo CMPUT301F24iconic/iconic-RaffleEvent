0b91ccbc7f50cb769aa17f1d838f141e46fe38f4
HetP1742431 <hetbhar@ualberta.ca>
2024-10-29 16:43:49 -0600

    Changes to the top and bottom nav bars and event details page

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
         classpath(libs.google.services)
     }
 }
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add_icon.xml configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.7.1" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
