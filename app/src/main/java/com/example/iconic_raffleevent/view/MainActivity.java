de95de0e02b9c3598d973d916d2be262dd192943
AidenTeal <ateal@ualberta.ca>
2024-10-29 22:30:26 -0600

    Added welcome/create user page if device id is not recognized

--- app/src/main/java/com/example/iconic_raffleevent/view/MainActivity.java
+++ app/src/main/java/com/example/iconic_raffleevent/view/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.iconic_raffleevent.view;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.iconic_raffleevent.R;
 import com.example.iconic_raffleevent.controller.UserController;
 import com.example.iconic_raffleevent.model.User;
+
+import java.util.Set;
 
 /**
  * MainActivity serves as the home screen of the application.
@@ -27,6 +32,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+         Get Device ID and check if there is an existing user with that ID
+         */
+        String deviceID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        System.out.println("Device ID: " + deviceID);
+
+        userController = new UserController(deviceID);
+        userController.getUserInformation(new UserController.UserFetchCallback() {
+            @Override
+            public void onUserFetched(User user) {
+                if (user == null) {
+                    // User null, redirect to create user page to make a user
+                    startActivity(new Intent(MainActivity.this, NewUserActivity.class));
+                }
+            }
+
+            @Override
+            public void onError(String message) {
+                System.out.println("There was an error");
+            }
+        });
 
         profileButton = findViewById(R.id.profile_button);
         eventsButton = findViewById(R.id.events_button);
