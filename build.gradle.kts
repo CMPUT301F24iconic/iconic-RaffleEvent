1c1d1ad1cdec5a3efa90471f359c963f99bc25c1
AidenTeal <ateal@ualberta.ca>
2024-12-02 14:25:24 -0700

    Updated build gradle version to work on all devices

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,6 @@ buildscript {
 }
 // Top-level build file where you can add_icon.xml configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.2" apply false
+    id("com.android.application") version "8.6.1" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
